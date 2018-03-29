96b5abcce129b598758513ee56a402a18fe48d3c
Integradora1-PGutierrez <pgutierrezsantana@gmail.com>
2018-03-28 23:54:01 -0600

    spring 2

--- app/src/main/java/com/example/paola/unidad2pggs/MainActivity.java
+++ app/src/main/java/com/example/paola/unidad2pggs/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.paola.unidad2pggs;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,27 +14,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void nextActivity1(View view){
-        startActivity(new Intent(this, RouteDH.class));
+        startActivity(new Intent(this, RouteDhActivity.class));
     }
 
     public void nextActivity2(View view){
-        startActivity(new Intent(this, PhotoDH.class));
+        startActivity(new Intent(this, PhotoGalleryActivity.class));
     }
 
     public void nextActivity3(View view){
-        startActivity(new Intent(this, ParkingDH.class));
+        startActivity(new Intent(this, ParkingDhActivity.class));
     }
 
     public void nextActivity4(View view){
-        startActivity(new Intent(this, History.class));
+        startActivity(new Intent(this, HistoryActivity.class));
     }
 
     public void nextActivity5(View view){
-        startActivity(new Intent(this, Statistic.class));
+        startActivity(new Intent(this, StatisticActivity.class));
     }
 
     public void nextActivity6(View view){
-        startActivity(new Intent(this, Crafts.class));
+        startActivity(new Intent(this, CraftsVideoActivity.class));
     }
     
 }
